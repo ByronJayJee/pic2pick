3e039b3bc5df4824c96fccdb72615dcab29e7f82
ByronJayJee <ByronJayJee@gmail.com>
2019-08-15 16:51:35 -0400

    added site skeleton using Twitter Bootstrap

---
+++ bot-app/site/js/image_server.js
@@ -0,0 +1,14 @@
+// Make Pop-up on click
+function clickPopup() {
+	alert('test');
+}
+
+
+// Select image by clicking on card
+function selectImage(img_num_str) {
+	//var tmp_str = 'Image ' + toString(Number(img_num))
+	console.log('img_num_str',img_num_str)
+	var tmp_str = 'Image ' + img_num_str + ' selected'
+	alert(tmp_str);
+}
+
